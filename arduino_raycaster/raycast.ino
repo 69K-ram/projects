393c66f1be3356bf112a87eb3e3b205a784c65ad
69K-ram <66765456+69K-ram@users.noreply.github.com>
2020-08-07 12:16:54 -0700

    Update raycast.ino

--- arduino_raycaster/raycast.ino
+++ arduino_raycaster/raycast.ino
@@ -1,1 +1,261 @@
+//A raycast project I worked on.
+//Credit to https://lodev.org/cgtutor/raycasting.html for the original code
 
+#include <SPI.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_PCD8544.h>
+
+// 1 - Serial clock out (SCLK)
+// 2 - Serial data out (DIN)
+// 3 - Data/Command select (D/C)
+// 4 - LCD chip select (CS)
+// 5 - LCD reset (RST)
+Adafruit_PCD8544 display = Adafruit_PCD8544(D4, D3, D2, D0, D1);
+
+#define screenWidth 84
+#define screenHeight 48
+#define mapWidth 24
+#define mapHeight 24
+
+int worldMap[mapWidth][mapHeight] =
+{
+  {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
+  {1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+  {1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+  {1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+  {1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+  {1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+  {1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+  {1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+  {1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+  {1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+  {1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+  {1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+  {1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+  {1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+  {1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+  {1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+  {1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+  {1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+  {1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+  {1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+  {1, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+  {1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+  {1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+  {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1}
+};
+
+void game() {
+  double posX = 10, posY = 10;  //x and y start position
+  double dirX = -1, dirY = 0; //initial direction vector
+  double planeX = 0, planeY = 0.6; //the 2d raycaster version of camera plane
+
+  double gameTime = 0; //time of current frame
+  double oldGameTime = 0; //time of previous frame
+
+  bool done = false;
+  int w = 84;
+  int h = 48;
+
+  bool forwards = false;
+  bool backwards = false;
+  bool left = false;
+  bool right = false;
+
+  int oldLineHeight;
+  int oldDrawStart;
+  int oldDrawEnd;
+  int idkMathShitInt;
+  int idkMathShitInt2;
+
+  while (done != true) {
+    //
+    for (int x = 0; x < w; x++)
+    {
+      //calculate ray position and direction
+      double cameraX = 2 * x / (double)w - 1; //x-coordinate in camera space
+      double rayDirX = dirX + planeX * cameraX;
+      double rayDirY = dirY + planeY * cameraX;
+      //which box of the map we're in
+      int mapX = int(posX);
+      int mapY = int(posY);
+
+      //length of ray from current position to next x or y-side
+      double sideDistX;
+      double sideDistY;
+
+      //length of ray from one x or y-side to next x or y-side
+      double deltaDistX = abs(1 / rayDirX);
+      double deltaDistY = abs(1 / rayDirY);
+      double perpWallDist;
+
+      //what direction to step in x or y-direction (either +1 or -1)
+      int stepX;
+      int stepY;
+
+      int hit = 0; //was there a wall hit?
+      int side; //was a NS or a EW wall hit?
+      //calculate step and initial sideDist
+      if (rayDirX < 0) {
+        stepX = -1;
+        sideDistX = (posX - mapX) * deltaDistX;
+      }
+      else {
+        stepX = 1;
+        sideDistX = (mapX + 1.0 - posX) * deltaDistX;
+      }
+      if (rayDirY < 0) {
+        stepY = -1;
+        sideDistY = (posY - mapY) * deltaDistY;
+      }
+      else {
+        stepY = 1;
+        sideDistY = (mapY + 1.0 - posY) * deltaDistY;
+      }
+      //perform DDA
+      while (hit == 0) {
+        //jump to next map square, OR in x-direction, OR in y-direction
+        if (sideDistX < sideDistY) {
+          sideDistX += deltaDistX;
+          mapX += stepX;
+          side = 0;
+        }
+        else {
+          sideDistY += deltaDistY;
+          mapY += stepY;
+          side = 1;
+        }
+        //Check if ray has hit a wall
+        if (worldMap[mapX][mapY] > 0) hit = 1;
+      }
+      //Calculate distance projected on camera direction (Euclidean distance will give fisheye effect!)
+      if (side == 0) perpWallDist = (mapX - posX + (1 - stepX) / 2) / rayDirX;
+      else          perpWallDist = (mapY - posY + (1 - stepY) / 2) / rayDirY;
+
+      //Calculate height of line to draw on screen
+      int lineHeight = (int)(h / perpWallDist);
+
+      //calculate lowest and highest pixel to fill in current stripe
+      int drawStart = -lineHeight / 2 + h / 2;
+      if (drawStart < 0)drawStart = 0;
+      int drawEnd = lineHeight / 2 + h / 2;
+      if (drawEnd >= h)drawEnd = h - 1;
+
+      //choose wall color
+      int color;
+      switch (worldMap[mapX][mapY])
+      {
+        case 1:
+          display.drawPixel(x, drawStart, BLACK);
+          display.drawPixel(x, drawEnd, BLACK);
+          break;
+        case 2:  color = 2;
+          display.drawFastVLine(x, drawStart, lineHeight, BLACK);
+          break;
+        default: color = 1;
+          display.drawPixel(x, drawStart, BLACK);
+          display.drawPixel(x, drawEnd, BLACK);
+          break;
+      }
+      //draw the pixels of the stripe as a vertical line
+      if (oldDrawStart != drawStart) {
+        display.drawFastVLine((x - 1), oldDrawStart, oldLineHeight, BLACK);
+      }
+      oldLineHeight = lineHeight;
+      oldDrawStart = drawStart;
+      oldDrawEnd = drawEnd;
+    }
+    display.display();
+    display.fillScreen(WHITE);
+    if (dirY > 0) {
+      while (idkMathShitInt2 < 10) {
+        idkMathShitInt = (screenWidth / 10) + sin(dirY);
+        display.drawLine(0, dirY * (screenWidth / 10), screenWidth, -(dirY * (screenWidth / 10)), BLACK);
+        idkMathShitInt2 ++;
+      }
+    }
+    else if (dirY < 0) {
+      while (idkMathShitInt2 < 10) {
+        idkMathShitInt = (screenWidth / 10) + sin(dirY);
+        display.drawLine(0, -(dirY * (screenWidth / 10)), screenWidth, dirY * (screenWidth / 10), BLACK);
+        idkMathShitInt2 ++;
+      }
+    }
+    //clear top half of screen
+    //display.fillRect(0, 0, 48, 24, WHITE);
+
+    //timing for input and FPS counter
+    oldGameTime = gameTime;
+    gameTime = millis();
+    double frameTime = (gameTime - oldGameTime) / 1000.0; //frameTime is the time this frame has taken, in seconds
+    //Serial.print(1.0 / frameTime); //FPS counter
+    //Serial.println(" FPS");
+    Serial.print("X");
+    Serial.println(dirX);
+    Serial.print("Y");
+    Serial.println(dirY);
+    //display.display();
+
+    //speed modifiers
+    double moveSpeed = frameTime * 5.0; //the constant value is in squares/second
+    double rotSpeed = frameTime * 3.0; //the constant value is in radians/second
+    //forwards
+    if (digitalRead(D7) == LOW) {
+      forwards = true;
+    }
+    else forwards = false;
+    //left
+    if (digitalRead(D5) == LOW) left = true;
+    else left = false;
+    //right
+    if (digitalRead(D6) == LOW) right = true;
+    else right = false;
+
+    //move forward if no wall in front of you
+    if (forwards == true) {
+      if (worldMap[int(posX + dirX * moveSpeed)][int(posY)] == false) posX += dirX * moveSpeed;
+      if (worldMap[int(posX)][int(posY + dirY * moveSpeed)] == false) posY += dirY * moveSpeed;
+    }
+    //move backwards if no wall behind you
+    if (backwards == true) {
+      if (worldMap[int(posX - dirX * moveSpeed)][int(posY)] == false) posX -= dirX * moveSpeed;
+      if (worldMap[int(posX)][int(posY - dirY * moveSpeed)] == false) posY -= dirY * moveSpeed;
+    }
+    //rotate to the right
+    if (right == true) {
+      //both camera direction and camera plane must be rotated
+      double oldDirX = dirX;
+      dirX = dirX * cos(-rotSpeed) - dirY * sin(-rotSpeed);
+      dirY = oldDirX * sin(-rotSpeed) + dirY * cos(-rotSpeed);
+      double oldPlaneX = planeX;
+      planeX = planeX * cos(-rotSpeed) - planeY * sin(-rotSpeed);
+      planeY = oldPlaneX * sin(-rotSpeed) + planeY * cos(-rotSpeed);
+    }
+    //rotate to the left
+    if (left == true) {
+      //both camera direction and camera plane must be rotated
+      double oldDirX = dirX;
+      dirX = dirX * cos(rotSpeed) - dirY * sin(rotSpeed);
+      dirY = oldDirX * sin(rotSpeed) + dirY * cos(rotSpeed);
+      double oldPlaneX = planeX;
+      planeX = planeX * cos(rotSpeed) - planeY * sin(rotSpeed);
+      planeY = oldPlaneX * sin(rotSpeed) + planeY * cos(rotSpeed);
+    }
+  }
+}
+void setup() {
+  // put your setup code here, to run once:
+  display.begin();
+  display.setContrast(50);
+  display.setBias(0x05);
+  Serial.begin(9600);
+  pinMode(D5, INPUT_PULLUP);
+  pinMode(D6, INPUT_PULLUP);
+  pinMode(D7, INPUT_PULLUP);
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  game();
+}
